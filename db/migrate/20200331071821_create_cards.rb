ce353dec49e70149bb5fd969b93612d5cc077168
sho1374k <sho4771k@gmail.com>
2020-03-31 16:36:17 +0900

    commit-commit

---
+++ db/migrate/20200331071821_create_cards.rb
@@ -0,0 +1,13 @@
+class CreateCards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cards do |t|
+      t.references :user,      null: false, foreign_key: true
+      t.references :card,      null: false, foreign_key: true
+      t.string :token,         null: false
+      t.references :customer,  null: false
+
+      
+      t.timestamps
+    end
+  end
+end
