d5bede7c9bccc679489d1b4c05893e5fd516ca58
nishionemui <anlihvosion227@gmail.com>
2020-04-01 21:05:51 +0900

    003

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,14 +4,14 @@ class Product < ApplicationRecord
   # has_many :images, dependent: :destoroy  
   has_many :images
   has_many :comments, dependent: :destroy
-  belongs_to_active_hash :brand
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :size
-  belongs_to_active_hash :delivery_fee
-  belongs_to_active_hash :delivery_date
-  belongs_to_active_hash :delivery_way
-  belongs_to_active_hash :prefecture
+  # belongs_to_active_hash :brand
+  # belongs_to_active_hash :condition
+  # belongs_to_active_hash :size
+  # belongs_to_active_hash :delivery_fee
+  # belongs_to_active_hash :delivery_date
+  # belongs_to_active_hash :delivery_way
+  # belongs_to_active_hash :prefecture
 
-  add_index :products, :product_name
+  # add_index :products, :product_name
 
 end
