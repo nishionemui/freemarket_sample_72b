e9fc72cc1ddaa374e87712ee734a6277ff4dc39f
sho1374k <sho4771k@gmail.com>
2020-04-01 20:51:48 +0900

    devise-miss

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # validates :nickname, presence: true
+
   has_many :products, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one :address
