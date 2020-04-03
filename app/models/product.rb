bdf83438c0d2aa25f63f69fe0f37bfb56114c78c
nishionemui <anlihvosion227@gmail.com>
2020-04-03 17:49:32 +0900

    400305

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  # extend ActiveHash::Associations::ActiveRecordExtensions
   
   belongs_to :user
   has_many :main_categories, dependent: :destroy
@@ -7,13 +7,13 @@ class Product < ApplicationRecord
   has_many :images
   has_many :comments, dependent: :destroy
 
-  belongs_to_active_hash :brand, presence: true
-  belongs_to_active_hash :condition, presence: true
-  belongs_to_active_hash :size, presence: true
-  belongs_to_active_hash :delivery_fee, presence: true
-  belongs_to_active_hash :delivery_date, presence: true
-  belongs_to_active_hash :delivery_way, presence: true
-  belongs_to_active_hash :prefecture, presence: true
+  # belongs_to_active_hash :brand, presence: true
+  # belongs_to_active_hash :condition, presence: true
+  # belongs_to_active_hash :size, presence: true
+  # belongs_to_active_hash :delivery_fee, presence: true
+  # belongs_to_active_hash :delivery_date, presence: true
+  # belongs_to_active_hash :delivery_way, presence: true
+  # belongs_to_active_hash :prefecture, presence: true
 
   # validates :brand, presence: true
   # validates :condition, presence: true
