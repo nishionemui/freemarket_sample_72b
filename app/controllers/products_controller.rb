163c3e20b6eda90735683d540b0817b34ebc9a54
van319 <bansyuukatu@gmail.com>
2020-04-03 15:29:01 +0900

    routeing010

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,9 @@
 class ProductsController < ApplicationController
   def index
-    
+
+    @product = Product.all
+
+   
   end
   
   def new
@@ -11,5 +14,11 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
+  end
+
+  private 
+  def product_params 
+    params.require (:product).permit(:product_name, :description, :brand_id,:condition_id, :delivery_fee_id, :delivery_date_id, :prefecture_id).merge(user_id: current_user.id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @uswer = User.create(use_params)
+  end
+
+  def update
+  
+  end
+
+
   def show
+    @user = User.find(params[:id])
+   
+  end
+
+  private 
+  def user_params 
+    require params(:product).parmit(:nickname, :first_name,:last_name,:first_name_read,:last_name_read,:phone_num, :birthday, :user_image)
   end
 end
