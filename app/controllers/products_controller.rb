0fd151629e178aaac81fd3fa55d2b5dc186b5acc
fujiken7777 <v.i.p.ordermade@gmail.com>
2020-04-07 10:53:49 +0900

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
   end
 
   def create
-    # @products = Product.create(product_params)
+    @products = Product.create(product_params)
   end
 
   def show
@@ -23,6 +23,6 @@ class ProductsController < ApplicationController
 
   private 
   def product_params 
-    params.require(:product).permit(:product_name, :description, :brand_id,:condition_id, :delivery_fee_id, :delivery_date_id, :delivery_way_id, :prefecture_id).merge(user_id: current_user.id)
+    params.require(:product).permit(:product_name, :description, :brand_id, :condition_id, :delivery_fee_id, :delivery_date_id, :delivery_way_id, :prefecture_id, :price, :size_id).merge(user_id: current_user.id)
   end
 end
