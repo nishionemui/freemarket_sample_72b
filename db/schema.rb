32dc9a313b4e1001e68593c83417d825aa1f6c72
nishionemui <anlihvosion227@gmail.com>
2020-04-08 14:06:36 +0900

    040802

--- db/schema.rb
+++ db/schema.rb
@@ -26,12 +26,10 @@ ActiveRecord::Schema.define(version: 2020_03_31_085823) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "card_id", null: false
-    t.bigint "customer_id", null: false
+    t.string "card_id", null: false
+    t.string "customer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["card_id"], name: "index_cards_on_card_id"
-    t.index ["customer_id"], name: "index_cards_on_customer_id"
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
@@ -54,7 +52,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_085823) do
   end
 
   create_table "main_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "product_id", null: false
+    t.bigint "product_id"
     t.string "main_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -124,6 +122,5 @@ ActiveRecord::Schema.define(version: 2020_03_31_085823) do
   end
 
   add_foreign_key "addresses", "users"
-  add_foreign_key "cards", "cards"
   add_foreign_key "cards", "users"
 end
