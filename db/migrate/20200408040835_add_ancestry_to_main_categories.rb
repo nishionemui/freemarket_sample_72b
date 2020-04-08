398bd1ef8ba08aec496309230e40dd21c817660e
nishionemui <anlihvosion227@gmail.com>
2020-04-08 14:08:43 +0900

    1

--- db/migrate/20200408040835_add_ancestry_to_main_categories.rb
+++
@@ -1,6 +0,0 @@
-class AddAncestryToMainCategories < ActiveRecord::Migration[5.2]
-  def change
-    add_column :main_categories, :ancestry, :string
-    add_index :main_categories, :ancestry
-  end
-end
