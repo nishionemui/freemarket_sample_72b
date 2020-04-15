30768c709f1bb3b52ac1eee61ec6ca66b7e0a359
sho1374k <sho4771k@gmail.com>
2020-04-15 11:30:57 +0900

    devise-code-reviews

--- spec/factories/address.rb
+++ spec/factories/address.rb
@@ -1,8 +1,8 @@
-# FactoryBot.define do
-#   factory :address do
-#     zip_code             {"123-4567"}
-#     prefecture_id        {"北海道"}
-#     city                 {"名古屋市"}
-#     address1             {"1-1"}
-#   end
-# end
+FactoryBot.define do
+  factory :address do
+    zip_code             {"123-4567"}
+    prefecture_id        {"北海道"}
+    city                 {"名古屋市"}
+    address1             {"1-1"}
+  end
+end
