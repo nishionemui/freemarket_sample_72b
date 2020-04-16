c40887ea505074701d7c63bc026d5aa565e75f9c
fujiken7777 <v.i.p.ordermade@gmail.com>
2020-04-16 18:49:26 +0900

    commiy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_072709) do
+ActiveRecord::Schema.define(version: 2020_04_16_065355) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "zip_code", null: false
@@ -84,7 +84,6 @@ ActiveRecord::Schema.define(version: 2020_04_16_072709) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
-    t.integer "likes_count"
     t.index ["condition_id"], name: "index_products_on_condition_id"
     t.index ["delivery_date_id"], name: "index_products_on_delivery_date_id"
     t.index ["delivery_fee_id"], name: "index_products_on_delivery_fee_id"
