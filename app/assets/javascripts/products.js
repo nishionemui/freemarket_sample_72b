2f17b7ac04f37abfc4c1c5187e96d2e20f04792a
sho1374k <sho4771k@gmail.com>
2020-04-17 16:16:56 +0900

    パンくず機能

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,35 @@
+$(function() {
+  $('#hover-a').hover(function() {
+    $(this).css('color', 'orange');
+  }, function() {
+    $(this).css('color', '');
+  });
+});
+$(function() {
+  $('#hover-b').hover(function() {
+    $(this).css('color', 'orange');
+  }, function() {
+    $(this).css('color', '');
+  });
+});
+$(function() {
+  $('#hover-c').hover(function() {
+    $(this).css('color', 'orange');
+  }, function() {
+    $(this).css('color', '');
+  });
+});
+$(function() {
+  $('#hover-d').hover(function() {
+    $(this).css('color', 'orange');
+  }, function() {
+    $(this).css('color', '');
+  });
+});
+$(function() {
+  $('#hover-e').hover(function() {
+    $(this).css('color', 'orange');
+  }, function() {
+    $(this).css('color', '');
+  });
+});
