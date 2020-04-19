b5a386cb526f657e443f34dd2b69e4e700ceb95c
fujiken7777 <v.i.p.ordermade@gmail.com>
2020-04-19 12:34:32 +0900

    commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,26 @@ class CommentsController < ApplicationController
   before_action :set_instance
 
   def create
-    @comment = Comment.create(comment_params)
-     respond_to do |format|
-      format.html { redirect_to product_path(params[:product_id])  }
-      format.json
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to product_path(params[:product_id])  }
+        format.json
+      end
+    else
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :show
     end
   end
 
   def destroy
-    @comment = Comment.find_by(id: params[:id])
-    @comment.destroy
-    redirect_to "/products/#{@product.id}"
+    @comment = Comment.find(id: params[:id])
+    if @comment.destroy
+      redirect_to product_path(@product.id)
+    else
+      flash.now[:alert] = 'メッセージ削除に失敗しました。'
+      render :show
+    end
   end
 
   private
