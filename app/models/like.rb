d833574a5fe14b652c0f6ca19db6e06017662718
nishionemui <anlihvosion227@gmail.com>
2020-04-21 12:13:47 +0900

    0421

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,5 +4,8 @@ class Like < ApplicationRecord
   belongs_to :product, counter_cache: :likes_count
   belongs_to :user
 
-
+  ransacker :likes_count do
+    query = '(SELECT COUNT(likes.product_id) FROM likes where likes.product_id = product.id GROUP BY likes.product_id)'
+    Arel.sql(query)
+  end
 end
